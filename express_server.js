140745cbd76aeb618dd64b9be5f15ed04369a7d9
Bana <banabatshon@hotmail.com>
2019-03-12 23:28:02 +0000

    posts a message 'ok' when a submission is made

--- express_server.js
+++ express_server.js
@@ -1,7 +1,13 @@
+function generateRandomString() {
+
+}
+
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -21,6 +27,17 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+// app.post("/urls", (req, res) => {
+//   console.log('req body:', JSON.stringify(req.body, null, 2));
+//   res.status(200).end();
+// })
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] /* What goes here? */ };
   res.render("urls_show", templateVars);
