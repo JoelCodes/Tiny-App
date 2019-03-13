a01586ee1d704be1fb0cba98c2faf1bf2386af2b
Bana <banabatshon@hotmail.com>
2019-03-13 03:40:21 +0000

    redirects the user to the short url when long url is submitted. Then again directed to the long url webpage when the user copies the short url generated by this project and pastes it in the web browser

--- express_server.js
+++ express_server.js
@@ -1,10 +1,16 @@
 function generateRandomString() {
+  var string = "";
+  var options = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
+  for (var i = 0; i < 5; i++)
+    string += options.charAt(Math.floor(Math.random() * options.length));
+
+  return string;
 }
 
 var express = require("express");
 var app = express();
-var PORT = 8080; // default port 8080
+var PORT = 8080;
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -32,16 +38,23 @@ app.get("/urls/new", (req, res) => {
 });
 app.post("/urls", (req, res) => {
   console.log(req.body);
-  res.send("Ok");
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL
+  console.log('urlDatabase:', JSON.stringify(urlDatabase, null, 2));
+  res.redirect("/urls/" + shortURL)
 });
 // app.post("/urls", (req, res) => {
 //   console.log('req body:', JSON.stringify(req.body, null, 2));
 //   res.status(200).end();
 // })
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] /* What goes here? */ };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], port: PORT };
   res.render("urls_show", templateVars);
 });
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});k
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
