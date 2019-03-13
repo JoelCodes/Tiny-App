2b30fe1412e97f349200cc581aece8030835a6b3
Bana <banabatshon@hotmail.com>
2019-03-13 22:36:59 +0000

    adds an edit anchor and redirects the user to an update page to update their long url

--- express_server.js
+++ express_server.js
@@ -18,15 +18,9 @@ var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
+app.get("/", (req, res) => {
+  res.redirect("/urls");
+});
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -42,8 +36,15 @@ app.get("/urls/new", (req, res) => {
 });
 app.get("/urls/:shortURL", (req, res) => {
   console.log(req.params);
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], port: PORT };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+app.post("/urls/:shortURL", (req, res) => {
+  console.log(req.params);
+  const updatedURL =req.body.updatedURL
+  urlDatabase[req.params.shortURL] = updatedURL;
+  res.redirect("/urls");
+  
 });
 app.get("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls")
